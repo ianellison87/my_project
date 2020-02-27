9e4560da7a6fcddf65c3db5c07400ae2c5699b65
ianellison <ianellison87@yahoo.com>
2020-02-26 21:16:07 -0800

    learning react native

---
+++ App.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+
+
+
+export default function App() {
+  const [outputText, setOutputText] = useState("How quickly does this change ?");
+  return (
+    <View style={styles.container}>
+      <Text>{outputText}</Text>
+      <Button title="Change Text" onPress={() => setOutputText('The text changed')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
